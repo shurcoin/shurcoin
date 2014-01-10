c69a1d02853c9f583c2454c6e252bdce264933ed
shurcoin <shurcoin@gmail.com>
2014-01-10 11:28:57 +0100

    Fixed ports in range.

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -39,7 +39,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return 66883;
+    return 62883;
 }
 
 Object JSONRPCError(int code, const string& message)
